e3ad9eca879e54f2a860e40127ee9c211723bd86
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-06-20 20:39:57 +0200

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// client/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './client',
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    host: true, // exposes to local network
+  },
+});
